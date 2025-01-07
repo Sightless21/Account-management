9118b685c37504aa3faa99f2e1b27429c7c445bc
Sightless21 <86817539+Sightless21@users.noreply.github.com>
2025-01-07 17:11:50 +0700

    Refactor Applicant dashboard layout; remove unused components and update metadata

--- src/app/dashboard/Applicant/page.tsx
+++ src/app/dashboard/Applicant/page.tsx
@@ -1,12 +1,13 @@
-import ApplicantorList from '@/components/applicantor-list'
-
+import React from "react";
+import { ApplicantBoard } from '@/components/applicant-board'
+import { Button } from "@/components/ui/button";
 export default function Page() {
     return (
         <>
-            <div>Applicant Dashboard</div>
-            <ApplicantorList />
+            <div className="flex justify-center items-center mb-6">
+                <Button className="bg-emerald-400 hover:bg-emerald-600">New Applicant</Button>
+            </div>
+            <ApplicantBoard />
         </>
-
-
     );
 }

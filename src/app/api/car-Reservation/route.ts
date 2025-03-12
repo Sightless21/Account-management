80b79b54d1b8b9ffe63f7c090d887c3f4d00ebb5
Sightless21 <86817539+Sightless21@users.noreply.github.com>
2025-03-12 13:40:56 +0700

    CHANGE 🔃: Refactor car reservation POST handler to streamline message sending via LINE; improve error handling and response structure for car reservation creation.

--- src/app/api/car-Reservation/route.ts
+++ src/app/api/car-Reservation/route.ts
@@ -3,8 +3,6 @@ import { NextResponse, NextRequest } from "next/server";
 import { sendLineMessage } from '@/utils/line';
 import { format } from 'date-fns';
 
-
-
 export async function GET() {
   try {
     const allCarReservation = await prisma.carReservation.findMany({
@@ -17,27 +15,26 @@ export async function GET() {
     console.error("Error fetching car reservation:", error);
     return NextResponse.json(
       { error: "Error fetching car reservation" },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const startDate = format(body.date.from, 'yyyy-MM-dd');
-  const endDate = format(body.date.to, 'yyyy-MM-dd');
-  if (startDate === endDate) {
-    sendLineMessage(`${body.car.plate} ถูกจองโดย 
-      คุณ ${body.employeeName} 
-    วันที่ ${startDate} ไปที่ ${body.destination}`);
-    return;
-  }
-  await sendLineMessage(`${body.car.plate} ถูกจองโดย 
-  คุณ ${body.employeeName} 
-  จากวันที่ ${startDate} ถึงวันที่ ${endDate} ไปที่ ${body.destination}`,);
-
   try {
+    const body = await req.json();
+    const startDate = format(body.date.from, 'yyyy-MM-dd');
+    const endDate = format(body.date.to, 'yyyy-MM-dd');
 
+    // ส่งข้อความ LINE ตามเงื่อนไข
+    const message =
+      startDate === endDate
+        ? `${body.car.plate} ถูกจองโดย คุณ ${body.employeeName} วันที่ ${startDate} ไปที่ ${body.destination}`
+        : `${body.car.plate} ถูกจองโดย คุณ ${body.employeeName} จากวันที่ ${startDate} ถึงวันที่ ${endDate} ไปที่ ${body.destination}`;
+    
+    await sendLineMessage(message);
+
+    // ค้นหารถ
     const car = await prisma.car.findFirst({
       where: {
         plate: body.car.plate.trim(),
@@ -45,9 +42,10 @@ export async function POST(req: NextRequest) {
     });
 
     if (!car) {
-      return NextResponse.json({ error: "Car creation failed" }, { status: 500 });
+      return NextResponse.json({ error: "Car not found" }, { status: 404 });
     }
 
+    // สร้าง Car Reservation
     const newCarReservation = await prisma.carReservation.create({
       data: {
         userId: body.userId,
@@ -63,8 +61,12 @@ export async function POST(req: NextRequest) {
         carId: car.id,
       },
     });
+
     console.log("✅ Created Car Reservation:", newCarReservation);
-    return NextResponse.json(newCarReservation, { status: 200 });
+    return NextResponse.json(
+      { message: "✅ Created Car Reservation", data: newCarReservation },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error creating car reservation:", error);
     return NextResponse.json(

ff9198d6396dcf17df4ca7f1fbee953f84e21172
Sightless21 <86817539+Sightless21@users.noreply.github.com>
2025-03-21 16:29:44 +0700

    CHANGE 🔃: FIX File name Uppercase and LowerCase

---
+++ src/components/Table/BookingRoom-table/BookingRoomTable.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { BookingDialog } from "@/components/Modal/modal-Booking"
+import { columns } from "./columns"
+import { DataTable } from "@/components/Table/Data-Table"
+import { useRoomBookings } from "@/hooks/useRoomBookingData"
+
+export default function BookingRoomTable() {
+  const { data: roombookings, isLoading, error } = useRoomBookings()
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error loading data</p>
+
+
+  return (
+    <div className="space-y-4">
+      <DataTable
+        columns={columns}
+        data={roombookings || []}
+        searchColumn="username"
+        searchPlaceholder="Search username..."
+        dateColumn="date"
+        defaultVisibleColumns={["username", "time", "date", "actions"]}
+        toolbarAdditionalControls={<BookingDialog />}
+      />
+    </div>
+  )
+}
+
